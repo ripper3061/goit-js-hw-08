ffa47348eb3d83897d368ef71d8da03be6550e5a
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-15 10:53:36 +0300

    добавление и импорт вимео плеера

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', function () {
+  console.log(timeupdate);
+});
