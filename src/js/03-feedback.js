8b78ec0936c83ebfd5c7fe0e15d92cc0b5e78afe
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-18 13:26:35 +0300

    Task3 Complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,30 +4,37 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const textArea = document.querySelector('.feedback-form textarea');
 
-let savedData = {};
 const STORAGE_KEY = 'feedback-form-state';
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+let formData = {
+  email: savedData ? savedData.email : '',
+  message: savedData ? savedData.message : '',
+};
 
 form.addEventListener('input', throttle(getSavedDataToLs, 500));
 form.addEventListener('submit', onFormSubmit);
 
-populateTextarea();
+verifyTextarea();
 
 function getSavedDataToLs(e) {
-  savedData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function populateTextarea() {
-  const savedD = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-  if (savedD) {
-    email.value = savedD.email;
-    textArea.value = savedD.message;
+function verifyTextarea() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    email.value = savedData.email;
+    textArea.value = savedData.message;
   }
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedData) {
+    console.log(savedData);
+  }
 
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
