e8d0ba6c2eeea7b6b11f2bf1dc56f45e36979957
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-18 12:43:20 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textArea = document.querySelector('.feedback-form textarea');
+
+let savedData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(getSavedDataToLs, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateTextarea();
+
+function getSavedDataToLs(e) {
+  savedData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function populateTextarea() {
+  const savedD = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedD) {
+    email.value = savedD.email;
+    textArea.value = savedD.message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
